911c772e8c9285c7d1c1a9a0b2bc201d1c82dbb6
NathanHardcastleOfficial <nathanjhardcastle@gmail.com>
2024-04-29 10:16:08 +0100

    Signed-off-by: NathanHardcastleOfficial <nathanjhardcastle@gmail.com>

--- application/accounts/urls.py
+++ application/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.views.generic.base import TemplateView 
 from .views import SignupFormView, EmployerSignupFormView, RankingsView
+from . import views
 
 
 urlpatterns = [
     path("signup/", SignupFormView.as_view(), name="signup"),
     path("employersignup/", EmployerSignupFormView.as_view(), name="employersignup"),
     path("rankings/", RankingsView.as_view(), name="rankings"),
+    path("trophies/", views.trophies, name="trophies"),
     path("", TemplateView.as_view(template_name="accountPage.html"), name="accountHome"),
 ]
